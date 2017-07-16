39fc731a6be669178a1945ca0bb382b6f3a9a746
Carrie Ho <carrie503.ch@gmail.com>
2017-07-16 21:10:40 +0800

    no message

--- cloudcode.py
+++ cloudcode.py
@@ -108,3 +108,39 @@ def update_writer(detail):
             if e.__class__.__name__ == 'IntegrityError' and e.orig.pgcode == '23505':
                 raise Exception('An account already exists with this email address.')
             else:
+
+@skygear.op("test-run", user_required=True)
+def update_writer(detail):
+    with _get_engine().begin() as conn:
+        trans = conn.begin()
+        try:
+            update_skygear_user_email(conn, detail['recordId'], detail['email'])
+            conn.execute(sa.text('''
+                UPDATE "{schema}"."TaylorsUser"
+                SET "email" = :email,
+                    "name" = :name,
+                    "address" = :address,
+                    "postalCode" = :postalCode,
+                    "city" = :city,
+                    "country" = :country,
+                    "contactNo" = :contactNo
+                WHERE _id = :id
+                '''.format(
+                    schema=db_schema,
+                )),
+                id=detail['recordId'],
+                email=detail['email'],
+                name=detail['name'],
+                address=detail['address'],
+                postalCode=detail['postalCode'],
+                city=detail['city'],
+                country=detail['country'],
+                contactNo=detail['contactNo']
+            )
+            trans.commit()
+        except Exception as e:
+            print("Unexpected error:", sys.exc_info()[0])
+            trans.rollback()
+            if e.__class__.__name__ == 'IntegrityError' and e.orig.pgcode == '23505':
+                raise Exception('An account already exists with this email address.')
+            else:
